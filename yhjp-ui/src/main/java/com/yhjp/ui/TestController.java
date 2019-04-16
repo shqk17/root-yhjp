51b22af5b401938bc82661ba5cb65bfb7b13e95f
fengshuhao <fengshuhao@rybbaby.com>
2019-04-16 16:53:45 +0800

    PDF export

--- yhjp-ui/src/main/java/com/yhjp/ui/TestController.java
+++ yhjp-ui/src/main/java/com/yhjp/ui/TestController.java
@@ -1,5 +1,12 @@
 package com.yhjp.ui;
 
+import com.lowagie.text.pdf.BaseFont;
+import org.dom4j.io.HTMLWriter;
+import org.dom4j.io.OutputFormat;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.W3CDom;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -7,26 +14,81 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.xhtmlrenderer.pdf.ITextFontResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xhtmlrenderer.simple.PDFRenderer;
+import org.xml.sax.InputSource;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.URL;
+
+import static org.jsoup.nodes.Document.OutputSettings.Syntax.html;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
     @RequestMapping("/ee")
-    public  String index(Model model){
-        return  "index";
+    public String index(Model model) {
+        return "index";
     }
-
     @RequestMapping("/bb")
-    public  String index1(Model model, ModelMap map){
-    System.out.println("say hi ……");
+    public String index1(Model model, ModelMap map) {
+        System.out.println("say hi ……");
 
         map.put("name", "kimi");
-    return  "index.ftl";
+        return "index.ftl";
     }
+    @RequestMapping("/indexs")
+    public String indexs(Model model, ModelMap map) {
+        return "indexs.ftl";
+    }
+
     @ResponseBody
-    @RequestMapping(value = "/savePdf",method = RequestMethod.POST)
-    public  String savePdf(Model model, @RequestBody String allHtml){
-      System.out.println(allHtml);
-        return  "success";
+    @RequestMapping(value = "/savePdfV2", method = RequestMethod.POST)
+    public void savePdfV2(Model model, @RequestBody String allHtml, HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "测试PDF111";
+        String body ="<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n" +
+                "\"http://www.w3.org/TR/html4/loose.dtd\">"+
+                "<html>"
+                + allHtml + "</body></html>";
+        try {
+            Document doc = Jsoup.parse(body,"UTF-8");
+            org.jsoup.helper.W3CDom w3cDom = new W3CDom();
+            org.w3c.dom.Document xhtmlContent = w3cDom.fromJsoup(doc);
+            ServletOutputStream sos = null;
+            ITextRenderer renderer = new ITextRenderer();
+            ITextFontResolver fontResolver = (ITextFontResolver) renderer
+                    .getSharedContext().getFontResolver();
+            URL ur = TestController.class.getClassLoader().getResource("../../WEB-INF/static/font");
+            File f = new File(ur.toURI());
+            if (f.isDirectory()) {
+                File[] files = f.listFiles(new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                        String lower = name.toLowerCase();
+                        return lower.endsWith(".otf") || lower.endsWith(".ttf") || lower.endsWith(".ttc");
+                    }
+                });
+                for (int i = 0; i < files.length; i++) {
+                    fontResolver.addFont(files[i].getAbsolutePath(), BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+                }
+            }
+            renderer.setDocument(xhtmlContent, "");
+            renderer.layout();
+            response.setContentType("application/pdf;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + ".pdf" + "\"");
+            sos = response.getOutputStream();
+            renderer.createPDF(sos);
+            renderer.finishPDF();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- yhjp-ui/src/main/webapp/WEB-INF/ftl/index.ftl
+++ yhjp-ui/src/main/webapp/WEB-INF/ftl/index.ftl
@@ -4,17 +4,15 @@
     <meta charset="UTF-8">
     <title>主页测试11</title>
 </head>
-<script src="http://localhost:8080/static/js/jquery-3.3.1.min.js"></script>
-<body>
-测试11
-
+<script src="http://localhost:8082/static/js/jquery-3.3.1.min.js"></script>
+<body style="font-family: SimSun; font-size:small;">
+测试11 fsdifh发低速合法IP速度回复U盾发哈皮U盾挂号费皮u刮大风呲牙的出发点从部分阿大V重复as大V发
+as给对方擦US大V发阿深V大幅阿斯钢大V发阿萨德股份via啥都有分割按地方是爱上对方
 <pre class="java" name="code"><html>
 <body>
     <h1>holle world ${name}</h1><br/>
-
     1 + 1 = 3? ${(1 != 1)?string("yes", "no")}
 </body>
-    
 </html>
 </pre>
 <br>
@@ -23,12 +21,11 @@
 <script>
     function getAllHtml() {
         var html = document.getElementsByTagName('html')[0].innerHTML;
-
         $.ajax({
-            url: "http://localhost:8080/test/savePdf/",
+            url: "http://localhost:8082/test/savePdfV2/",
             type: "POST",
             contentType: "application/json;charset=utf-8",
-            data: JSON.stringify({'allHtml': html}),
+            data: html,
             dataType: "text",
             success: function (result) {
                 if (result == "success") {
@@ -42,9 +39,5 @@
         })
         return false;
     }
-</script>
-    }
-    
-    
 </script>
 </html>
